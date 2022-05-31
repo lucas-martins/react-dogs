afee45838c28d8557a397b26982b28ebaaac218f
Lucas <lucas.martins@spassu.com.br>
2022-05-31 19:48:55 -0300

    [ADD] Skeleton Image

--- src/components/Feed/FeedPhotoItem.js
+++ src/components/Feed/FeedPhotoItem.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Image from '../Helpers/Image'
 import styles from './FeedPhotosItem.module.css'
 
 const FeedPhotoItem = ({photo, setModalPhoto}) => {
@@ -9,8 +10,8 @@ const FeedPhotoItem = ({photo, setModalPhoto}) => {
 
   return (
     <li className={styles.photo} onClick={handleClick}>
-        <img src={photo.src} alt={photo.title} />
-        <span className={styles.visualizacao}>{photo.acessos}</span>
+      <Image src={photo.src} alt={photo.title} />
+      <span className={styles.visualizacao}>{photo.acessos}</span>
     </li>
   )
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,9 +3,10 @@ import Feed from './Feed/Feed'
 
 const Home = () => {
     return (
-    <section className='container mainContainer'>
-            <Feed />
-        </section>
+			<section className='container mainContainer'>
+				{/* <Loading /> */}
+				<Feed />
+			</section>
     )
 }
 
