0b3df3dce20e491def520d0cdcdde08967e649df
Lucas <lucas.martins@spassu.com.br>
2022-06-29 20:08:33 -0300

    [ADD] Footer

--- src/App.js
+++ src/App.js
@@ -14,22 +14,24 @@ import NotFound from './components/NotFound';
 
 function App() {
   return (
-    <div>
+    <div className='App'>
       <BrowserRouter>
         <UserStorage>
           <Header />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="login/*" element={<Login />} />
-            <Route path="photo/:id" element={<Photo />} />
-            <Route path="perfil/:user" element={<UserProfile />} />
-            <Route path="conta/*" element={
-              <ProtectedRoute>
-                <User />
-              </ProtectedRoute>
-            } />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
+          <main className='AppBody'>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="login/*" element={<Login />} />
+              <Route path="photo/:id" element={<Photo />} />
+              <Route path="perfil/:user" element={<UserProfile />} />
+              <Route path="conta/*" element={
+                <ProtectedRoute>
+                  <User />
+                </ProtectedRoute>
+              } />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </main>
           <Footer />
         </UserStorage>
       </BrowserRouter>
